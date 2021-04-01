e50bd16e5fc01884cf433de563bf6773ed72d0b1
Khan Tanveer <khantanveer7977@gmail.com>
2021-04-01 11:18:56 +0530

    initial commit

--- pages/api/name.js
+++ pages/api/name.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import data from "../../data/Names.json";
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+  res.status(200).json(data);
+};
